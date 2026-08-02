a187964a7efe8a4c49724c97c0f511ef730e10c6
KHEA  VANNA <kheavanna0@gmail.com>
2026-08-02 11:01:55 +0700

    fix(brands): resolve merge conflict in Brands entity

--- src/main/java/com/saranaresturantsystem/entities/catalog/Brands.java
+++ src/main/java/com/saranaresturantsystem/entities/catalog/Brands.java
@@ -6,26 +6,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.List;
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "tbl_brands")
 public class Brands extends BaseEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(length = 50, nullable = false, unique = true)
+    private  Long id;
+    @Column(length = 50 , nullable = false , unique = true)
     private String name;
-
-    @Column(length = 500, nullable = false)
+    @Column(length = 50 , nullable = false , unique = true)
     private String imageUrl;
-
     private String status;
 
 }
